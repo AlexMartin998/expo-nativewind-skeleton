0f989a6f65e9072811ca156b37b59916a3b2594d
Adrian <alextravc.342@gmail.com>
2024-12-29 09:47:21 -0500

    ADD: skeleton v1 - NativeWind config

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View className="mt-10">
+      <Text className="text-2xl text-center text-blue-500">App</Text>
+    </View>
+  );
+};
+
+export default App;
